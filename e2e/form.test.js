45259ad7bd3550b42d7eed4a7086910e0ccb5bd4
Malins062 <6328673@gmail.com>
2022-11-08 17:47:49 +0300

    Create pupeter tests

---
+++ e2e/form.test.js
@@ -0,0 +1,42 @@
+import puppeteer from 'puppeteer';
+
+describe('Card number Form', () => {
+  let browser;
+  let page;
+
+  beforeEach(async () => {
+    browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 100,
+      devtools: true,
+    });
+
+    page = await browser.newPage();
+  });
+
+  test('Form should render on page start', async () => {
+    await page.goto('http://localhost:9000');
+
+    await page.waitFor('.innogrn-form-widget');
+  });
+
+  test('Form input should add .valid class if card number is valid', async () => {
+    jest.setTimeout(20000);
+    await page.goto('http://localhost:9000');
+
+    await page.waitFor('.innogrn-form-widget');
+
+    const form = await page.$('.innogrn-form-widget');
+    const input = await form.$('.input');
+    const submit = await form.$('.submit');
+
+    await input.type('2200240768512994');
+    await submit.click();
+
+    await page.waitFor('.innogrn-form-widget .input.valid');
+  });
+
+  afterEach(async () => {
+    await browser.close();
+  });
+});
