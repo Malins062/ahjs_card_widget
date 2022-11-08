efb40c6491cd10a55bf95fd4e85f7c64ae1f8468
Malins062 <6328673@gmail.com>
2022-11-08 17:15:25 +0300

    Create tests for widget

--- src/js/__tests__/dom.test.js
+++ src/js/__tests__/dom.test.js
@@ -1,2 +1,29 @@
-test('should ...', () => {
+import CardNumberWidget from '../widget';
+
+test('should render self', () => {
+  document.body.innerHTML = '<div id="container"></div>';
+
+  const container = document.querySelector('#container');
+  const widget = new CardNumberWidget(container);
+
+  widget.bindToDOM();
+
+  expect(container.innerHTML).toEqual(CardNumberWidget.markup);
+});
+
+test('should validate input', () => {
+  document.body.innerHTML = '<div id="container"></div>';
+
+  const container = document.querySelector('#container');
+  const widget = new CardNumberWidget(container);
+
+  widget.bindToDOM();
+
+  const input = container.querySelector(CardNumberWidget.inputSelector);
+  input.value = '2200240768512994';
+
+  const submit = container.querySelector(CardNumberWidget.submitSelector);
+  submit.click();
+
+  expect(input.classList.contains('valid')).toBeTruthy();
 });
--- src/js/__tests__/validator.test.js
+++ src/js/__tests__/validator.test.js
@@ -1,2 +1,15 @@
-test('should ...', () => {
+import { isValidCard } from '../validators';
+
+test.each([
+  ['true for valid card number', '2200240768512994', true],
+  ['true for valid card number', '5346028387436812', true],
+  ['true for valid card number', '3529992726838059', true],
+  ['true for valid card number', '4556737586899855', true],
+  ['true for valid card number', '36471414338070', true],
+  ['false for invalid card number', '2200240768512991', false],
+  ['false for invalid card number', ' ', false],
+  ['false for invalid card number', ' assa', false],
+  ['false for invalid card number', '36471414338070 ', false],
+])(('it should be %s'), (_, input, expected) => {
+  expect(isValidCard(input)).toBe(expected);
 });
